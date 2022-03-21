dce5a42e570615b438140927159feef5c33cc6cd
jonatascaires <jonatas.invistech@gmail.com>
2022-03-20 22:05:35 -0300

    google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -22,7 +22,23 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Passion+One"
             rel="stylesheet"
           />
-          {/* <script src="//code-sa1.jivosite.com/widget/7Zp0kKp62J" async></script> */}
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </Head>
         <body>
           <Main />
